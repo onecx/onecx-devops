420803d04b8cd1985686ed6502d99b4d7aa61041
Andrej Petras <andrej@lorislab.org>
2023-07-27 17:59:06 +0200

    fix: parameter name

--- terraform-scripts/modules/github/files/files.tf
+++ terraform-scripts/modules/github/files/files.tf
@@ -4,6 +4,6 @@ resource "github_repository_file" "resources" {
   repository          = var.repository_name
   branch              = var.branch
   file                = each.key
-  content             = file(format("modules/%s/%s", var.template, each.key))
+  content             = file(format("modules/%s/%s", var.module, each.key))
   overwrite_on_create = true
 }
