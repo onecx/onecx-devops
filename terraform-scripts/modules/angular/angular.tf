6967423e1a19ec95dafcaa6e8e6431e2ad58f29a
Andrej Petras <andrej@lorislab.org>
2023-07-27 11:31:04 +0200

    feat: add angular module, update repo pipe template

--- terraform-scripts/modules/angular/angular.tf
+++ terraform-scripts/modules/angular/angular.tf
@@ -4,14 +4,14 @@ resource "github_repository_file" "changelog" {
   repository          =  var.repository_name
   branch              = "main"
   file                = ".github/changelog.yaml"
-  content             = file("modules/quarkus/.github/changelog.yaml")
+  content             = file("modules/angular/.github/changelog.yaml")
   overwrite_on_create = true
 }
 resource "github_repository_file" "dependabot" {
   repository          =  var.repository_name
   branch              = "main"
   file                = ".github/dependabot.yml"
-  content             = file("modules/quarkus/.github/dependabot.yml")
+  content             = file("modules/angular/.github/dependabot.yml")
   overwrite_on_create = true
 }
 
@@ -20,56 +20,56 @@ resource "github_repository_file" "build_branch" {
   repository          =  var.repository_name
   branch              = "main"
   file                = ".github/workflows/build-branch.yml"
-  content             = file("modules/quarkus/.github/workflows/build-branch.yml")
+  content             = file("modules/angular/.github/workflows/build-branch.yml")
   overwrite_on_create = true
 }
 resource "github_repository_file" "build_pr" {
   repository          =  var.repository_name
   branch              = "main"
   file                = ".github/workflows/build-pr.yml"
-  content             = file("modules/quarkus/.github/workflows/build-pr.yml")
+  content             = file("modules/angular/.github/workflows/build-pr.yml")
   overwrite_on_create = true
 }
 resource "github_repository_file" "build_release" {
   repository          =  var.repository_name
   branch              = "main"
   file                = ".github/workflows/build-release.yml"
-  content             = file("modules/quarkus/.github/workflows/build-release.yml")
+  content             = file("modules/angular/.github/workflows/build-release.yml")
   overwrite_on_create = true
 }
 resource "github_repository_file" "build" {
   repository          =  var.repository_name
   branch              = "main"
   file                = ".github/workflows/build.yml"
-  content             = file("modules/quarkus/.github/workflows/build.yml")
+  content             = file("modules/angular/.github/workflows/build.yml")
   overwrite_on_create = true
 }
-resource "github_repository_file" "quarkus_create_fix_branch" {
+resource "github_repository_file" "create_fix_branch" {
   repository          =  var.repository_name
   branch              = "main"
   file                = ".github/workflows/create-fix-branch.ym"
-  content             = file("modules/quarkus/.github/workflows/create-fix-branch.yml")
+  content             = file("modules/angular/.github/workflows/create-fix-branch.yml")
   overwrite_on_create = true
 }
-resource "github_repository_file" "quarkus_create_release" {
+resource "github_repository_file" "create_release" {
   repository          =  var.repository_name
   branch              = "main"
   file                = ".github/workflows/create-release.yml"
-  content             = file("modules/quarkus/.github/workflows/create-release.yml")
+  content             = file("modules/angular/.github/workflows/create-release.yml")
   overwrite_on_create = true
 }
-resource "github_repository_file" "quarkus_documentation" {
+resource "github_repository_file" "documentation" {
   repository          =  var.repository_name
   branch              = "main"
   file                = ".github/workflows/documentation.yml"
-  content             = file("modules/quarkus/.github/workflows/documentation.yml")
+  content             = file("modules/angular/.github/workflows/documentation.yml")
   overwrite_on_create = true
 }
-resource "github_repository_file" "quarkus_sonar_pr" {
+resource "github_repository_file" "sonar_pr" {
   repository          =  var.repository_name
   branch              = "main"
   file                = ".github/workflows/sonar-pr.yml"
-  content             = file("modules/quarkus/.github/workflows/sonar-pr.yml")
+  content             = file("modules/angular/.github/workflows/sonar-pr.yml")
   overwrite_on_create = true
 }
 
