2d5f8c536a5f2a2e7c8f08632c9bb8e3eff7cffd
Andrej Petras <andrej@lorislab.org>
2023-07-28 15:59:53 +0200

    fix: add sonar cloud provider version

--- terraform-scripts/modules/sonarcloud/versions.tf
+++ terraform-scripts/modules/sonarcloud/versions.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "5.32.0"
-    }
     sonarcloud = {
       source  = "rewe-digital/sonarcloud"
       version = "0.5.2"
