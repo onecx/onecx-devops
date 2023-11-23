a7332f5059b0baf108fa5570bcf208c3cff3c987
Andrej Petras <andrej@lorislab.org>
2023-11-23 17:07:28 +0100

    feat: add search config

---
+++ terraform-scripts/products/onecx-search-config/product.tf
@@ -0,0 +1,28 @@
+
+# TEAM ONECX-SEARCH-CONFIG
+module "onecx-search-config-team" {
+  source = "../../modules/github/team"
+  team_name        = "onecx-search-config"
+  team_description = "OneCx search config services team"
+  team_members_file_path   = "products/onecx-search-config/team.csv"
+}
+
+# ONECX-SEARCH-CONFIG
+module "onecx-search-config" {
+  source = "../../modules/product"
+  repository_name        = "onecx-search-config"
+  repository_description = "OneCx search config"
+  team_id                = module.onecx-search-config-team.team_id
+  team_name              = module.onecx-search-config-team.team_name
+  labels = {
+    onecx-search-config-svc = "0ea5e9"
+  }
+}
+
+# ONECX-SEARCH-CONFIG-SVC
+module "onecx-search-config-svc" {
+  source = "../../modules/quarkus"
+  repository_name        = "onecx-search-config-svc"
+  repository_description = "OneCx search config service"
+  team_id                = module.onecx-search-config-team.team_id
+}
