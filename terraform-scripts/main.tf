8d7e75a84599ae59d0d5a5a3907e7b43dd115b6e
Andrej Petras <andrej@lorislab.org>
2024-01-09 17:44:23 +0100

    feat: remove portal core

--- terraform-scripts/main.tf
+++ terraform-scripts/main.tf
@@ -16,10 +16,6 @@ module "onecx-k8s" {
   source = "./products/onecx-k8s"
 }
 
-module "onecx-core" {
-  source = "./products/onecx-core"
-}
-
 module "onecx-parameter" {
   source = "./products/onecx-parameter"
 }
