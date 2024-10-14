19f46b5e7206a161b8ed69e9133ffc1742f319ba
Andrej Petras <andrej@lorislab.org>
2024-10-14 08:07:36 +0200

    feat: add data orchestrator bff and ui

--- terraform-scripts/products/onecx-data-orchestrator/product.tf
+++ terraform-scripts/products/onecx-data-orchestrator/product.tf
@@ -22,3 +22,20 @@ module "onecx-data-orchestrator-operator" {
   repository_description = "OneCx data orchestrator operator"
   team_id                = module.onecx-data-orchestrator-team.team_id
 }
+
+
+# ONECX-DATA-ORCHESTRATOR-BFF
+module "onecx-data-orchestrator-bff" {
+  source = "../../modules/quarkus"
+  repository_name        = "onecx-data-orchestrator-bff"
+  repository_description = "OneCX data orchestrator BFF"
+  team_id                = module.onecx-data-orchestrator-team.team_id
+}
+
+# ONECX-DATA-ORCHESTRATOR-UI
+module "onecx-data-orchestrator-ui" {
+  source = "../../modules/angular"
+  repository_name        = "onecx-data-orchestrator-ui"
+  repository_description = "OneCX data orchestrator UI"
+  team_id                = module.onecx-data-orchestrator-team.team_id
+}
