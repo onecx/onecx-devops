2e1f8569cfe27d8fca4173ad454f46a4fd92389f
Andrej Petras <andrej@lorislab.org>
2025-11-05 15:58:56 +0100

    feat: product helm auto-merge check

--- terraform-scripts/modules/product/product.tf
+++ terraform-scripts/modules/product/product.tf
@@ -7,6 +7,8 @@ module "repository" {
   team_id                = var.team_id
   branch                 = var.branch
   project_repo           = true
+  archived               = var.archived
+  auto_merge             = var.auto_merge
 }
 
 # GITHUB REPOSITORY BRANCHES/PATTERNS
